c7875de49d89cd9c5c8fbe80b0708b8366cf5c43
Inês Lino <ineslino01@hotmail.com>
2018-05-31 21:51:41 +0100

    Pets app Version 2 with Content Provider

--- Pets/app/src/main/java/com/example/android/pets/data/PetDbHelper.java
+++ Pets/app/src/main/java/com/example/android/pets/data/PetDbHelper.java
@@ -3,8 +3,12 @@ package com.example.android.pets.data;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+
 import com.example.android.pets.data.PetContract.PetEntry;
 
+/**
+ * Database helper for Pets app. Manages database creation and version management.
+ */
 public class PetDbHelper extends SQLiteOpenHelper {
 
     public static final String LOG_TAG = PetDbHelper.class.getSimpleName();
@@ -12,6 +16,9 @@ public class PetDbHelper extends SQLiteOpenHelper {
     /** Name of the database file */
     private static final String DATABASE_NAME = "shelter.db";
 
+    /**
+     * Database version. If you change the database schema, you must increment the database version.
+     */
     private static final int DATABASE_VERSION = 1;
 
     /**
@@ -23,7 +30,9 @@ public class PetDbHelper extends SQLiteOpenHelper {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
-    /** This is called when the database is created for the first time. */
+    /**
+     * This is called when the database is created for the first time.
+     */
     @Override
     public void onCreate(SQLiteDatabase db) {
         // Create a String that contains the SQL statement to create the pets table
@@ -38,7 +47,9 @@ public class PetDbHelper extends SQLiteOpenHelper {
         db.execSQL(SQL_CREATE_PETS_TABLE);
     }
 
-    /** This is called when the database needs to be upgraded */
+    /**
+     * This is called when the database needs to be upgraded.
+     */
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         // The database is still at version 1, so there's nothing to do be done here.
